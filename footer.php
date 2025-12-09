8a108188ef2be2f93dba3c0f5dd6cd7694e6886c
awakenworlds <peterfaucher@gmail.com>
2025-12-09 22:05:39 +0545

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+    <!-- Footer -->
+    <div class="about-section">
+        <button id="about-toggle" class="about-toggle">More ▼</button>
+        <div id="about-content" class="about-content hidden">
+            © 2025 Peter Jones Faucher. All rights reserved. This website shall solely be used for educational purposes.<br>
+            Questions / comments? Email: info@jonesquiz.com
+        </div>
+    </div>
